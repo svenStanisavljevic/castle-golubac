cf05128a019d87bf121df3cd7530e12605f953b9
sven <svenstanisavljevic@gmail.com>
2024-11-24 19:45:34 +0100

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+document.getElementById("playGame").addEventListener("click", function() {
+  window.location.href = "game.html";
+});
+
+document.getElementById("settings").addEventListener("click", function() {
+  window.location.href = "settings.html";
+});
+
